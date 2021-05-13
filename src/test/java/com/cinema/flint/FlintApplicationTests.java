f809e8810a29c3976d9e5eca997c81c6a4c4c08b
unknown <lsagna1@myges.fr>
2021-05-13 02:55:08 +0200

    Project start

---
+++ src/test/java/com/cinema/flint/FlintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.flint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
